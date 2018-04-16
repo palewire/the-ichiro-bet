2f8b89bd9d6b59779453c29d3454da1abf002cd7
palewire <ben.welsh@gmail.com>
2018-04-15 22:38:52 -0700

    Try again

--- ichiro/managers.py
+++ ichiro/managers.py
@@ -7,13 +7,30 @@ class ProjectionManager(models.Manager):
     def by_date(self):
         with connection.cursor() as cursor:
             cursor.execute("""
+            SELECT
+                max_dates.projection
+                max_dates.date,
+                max_ab.ab
+            FROM (
                 SELECT
                     p.projection,
-                    MAX(date_trunc('day', p.datetime)),
-                    MAX(p.ab) as ab
+                    MAX(date_trunc('day', p.datetime)) as date
                 FROM ichiro_projection p
                 GROUP BY 1
-                ORDER BY 1, 2 ASC""")
+                ORDER BY 1, 2 ASC
+            ) as max_dates
+            INNER JOIN (
+                SELECT
+                    p.projection,
+                    date_trunc('day', p.datetime) as date,
+                    MAX(p.ab) as ab
+                FROM ichiro_projection p
+                GROUP BY 1, 2
+                ORDER BY 1, 2 ASC
+            ) as max_ab
+            ON max_dates.projection = max_ab.projection
+            AND max_dates.date = max_ab.date
+            """)
             return self._dictfetchall(cursor)
 
     def _dictfetchall(self, cursor):
